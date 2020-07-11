9f6a40a6a36819b9c2c0e08e1588d1dc40e1cf13
leye195 <leye195@naver.com>
2020-07-11 15:57:48 +0900

    #평점 막대기 그래프

--- practice3/assets/js/index.js
+++ practice3/assets/js/index.js
@@ -1,4 +1,5 @@
 (() => {
+  const infoBox = document.querySelector(".info-box");
   const outerWidth = 600;
   const outerHeight = 350;
   const margin = { left: 30, top: 60, right: 30, bottom: 60 };
@@ -18,22 +19,46 @@
     .attr("class", "graph");
   const xScale = d3.scaleBand().range([0, innerWidth]).padding([0.5]);
   const yScale = d3.scaleLinear().range([innerHeight, 0]);
+
+  const moveTooltip = (e) => {
+    const {
+      target: { dataset },
+      target,
+    } = e;
+    infoBox.classList.add("show");
+    infoBox.style.left = `${target.getBoundingClientRect().left - 2}px`;
+    infoBox.style.top = `${target.getBoundingClientRect().top - 20}px`; //`${330 + 250 - height.baseVal.value}px`;
+    infoBox.querySelector(".rating-count").textContent = dataset["value"];
+  };
+
+  const hideTooltip = (e) => {
+    infoBox.classList.remove("show");
+  };
+
   const drawAxis = () => {
-    const xAxis = d3
-      .select(".graph")
+    const xAxis = g
       .append("g")
       .attr("class", "xaix")
       .attr("transform", "translate(-25, 230)")
+      .transition()
+      .duration(1000)
       .call(d3.axisBottom(xScale));
-    const yAxis = d3
-      .select(".graph")
+    //xAxis
+    g.append("text")
+      .attr("text-anchor", "end")
+      .attr("x", innerWidth - 250)
+      .attr("y", innerHeight + 50)
+      .text("평점");
+    const yAxis = g
       .append("g")
       .attr("class", "yaix")
       .attr("transform", "translate(-25, 0)")
+      .transition()
+      .duration(1000)
       .call(d3.axisRight(yScale));
   };
+
   const render = (data) => {
-    console.log(data);
     const r = [];
     for (
       let i = d3.min(data, (d) => d[xColumn]);
@@ -45,7 +70,10 @@
     yScale.domain(d3.extent(data, (d) => d[yColumn]));
     drawAxis();
 
-    const g = svg.append("g").attr("transform", "translate(5,60)");
+    const g = svg
+      .append("g")
+      .attr("class", "rect-container")
+      .attr("transform", "translate(5,60)");
     const rect = g.selectAll("rect").data(data);
     rect
       .enter()
@@ -53,9 +81,26 @@
       .attr("x", (d) => xScale(d[xColumn]))
       .attr("y", (d) => yScale(d[yColumn]))
       .attr("width", xScale.bandwidth())
-      .attr("height", (d) => innerHeight - yScale(d[yColumn]));
+      .attr("height", (d) => innerHeight - yScale(d[yColumn]))
+      .attr("data-value", (d) => d[yColumn])
+      .attr("fill", "#0984e3")
+      //.attr("transform", "rotate(180 20 50)")
+      .append("animate")
+      .attr("attributeName", "height")
+      .attr("transform", "rotate(180deg)")
+      .attr("from", (d) => 0)
+      .attr("to", (d) => innerHeight - yScale(d[yColumn]))
+      .attr("dur", "0.8s")
+      .attr("fill", "freeze")
+      .append("animate");
+
     rect.exit().remove();
+
+    const rectContainer = document.querySelector(".rect-container");
+    rectContainer.addEventListener("mouseover", moveTooltip);
+    rectContainer.addEventListener("mouseleave", hideTooltip);
   };
+
   const init = () => {
     d3.csv("./assets/ratings.csv", (data) => {
       return {
@@ -69,7 +114,6 @@
         const tmp = d.filter((v) => v.rating === i);
         cleanedData.push({ rating: i, count: tmp.length });
       }
-      //console.log(cleanedData);
       render(cleanedData);
     });
   };
