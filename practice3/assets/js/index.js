f5a96a0b08bfdd8fdbe18b4293eec3eaeec296d7
leye195 <leye195@naver.com>
2020-07-17 03:17:42 +0900

    commit

--- practice3/assets/js/index.js
+++ practice3/assets/js/index.js
@@ -8,18 +8,6 @@
 
   const xColumn = "rating";
   const yColumn = "count";
-  const svg = d3
-    .select("body")
-    .append("svg")
-    .attr("width", outerWidth)
-    .attr("height", outerHeight);
-  const g = svg
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`)
-    .attr("class", "graph");
-  const xScale = d3.scaleBand().range([0, innerWidth]).padding([0.5]);
-  const yScale = d3.scaleLinear().range([innerHeight, 0]);
-
   const moveTooltip = (e) => {
     const {
       target: { dataset },
@@ -35,11 +23,11 @@
     infoBox.classList.remove("show");
   };
 
-  const drawAxis = () => {
+  const drawAxis = (g, xScale, yScale) => {
     const xAxis = g
       .append("g")
       .attr("class", "xaix")
-      .attr("transform", "translate(-25, 230)")
+      .attr("transform", `translate(-25, 230)`)
       .transition()
       .duration(1000)
       .call(d3.axisBottom(xScale));
@@ -47,8 +35,7 @@
     g.append("text")
       .attr("text-anchor", "end")
       .attr("x", innerWidth - 250)
-      .attr("y", innerHeight + 50)
-      .text("평점");
+      .attr("y", innerHeight + 50);
     const yAxis = g
       .append("g")
       .attr("class", "yaix")
@@ -58,7 +45,57 @@
       .call(d3.axisRight(yScale));
   };
 
-  const render = (data) => {
+  const renderHorizontal = (data) => {
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", outerWidth)
+      .attr("height", outerHeight);
+    const graph = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`)
+      .attr("class", "graph");
+    const xScale = d3.scaleLinear().range([0, innerWidth]);
+    const yScale = d3.scaleBand().range([0, innerHeight]).padding(0.5);
+    const r = [];
+    for (
+      let i = d3.min(data, (d) => d[xColumn]);
+      i <= d3.max(data, (d) => d[xColumn]);
+      i += 0.5
+    )
+      r.push(i);
+    xScale.domain(d3.extent(data, (d) => d[yColumn]));
+    yScale.domain(r);
+    drawAxis(graph, xScale, yScale);
+
+    const g = svg
+      .append("g")
+      .attr("class", "rect-container")
+      .attr("transform", "translate(5,60)");
+    const rect = g.selectAll("rect").data(data);
+    rect
+      .enter()
+      .append("rect")
+      .attr("x", (d) => xScale(0))
+      .attr("y", (d) => yScale(d[xColumn]))
+      .attr("height", yScale.bandwidth())
+      .attr("width", (d) => xScale(d[yColumn]))
+      .attr("data-value", (d) => d[yColumn])
+      .attr("fill", "#0984e3");
+  };
+
+  const renderVertical = (data) => {
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", outerWidth)
+      .attr("height", outerHeight);
+    const graph = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`)
+      .attr("class", "graph");
+    const xScale = d3.scaleBand().range([0, innerWidth]).padding(0.5);
+    const yScale = d3.scaleLinear().range([innerHeight, 0]);
     const r = [];
     for (
       let i = d3.min(data, (d) => d[xColumn]);
@@ -68,7 +105,7 @@
       r.push(i);
     xScale.domain(r);
     yScale.domain(d3.extent(data, (d) => d[yColumn]));
-    drawAxis();
+    drawAxis(graph, xScale, yScale);
 
     const g = svg
       .append("g")
@@ -114,7 +151,8 @@
         const tmp = d.filter((v) => v.rating === i);
         cleanedData.push({ rating: i, count: tmp.length });
       }
-      render(cleanedData);
+      renderVertical(cleanedData);
+      renderHorizontal(cleanedData);
     });
   };
   init();
